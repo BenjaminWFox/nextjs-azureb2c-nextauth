084c411c9eff518b65620ea58fd3ba96f76169f4
Ben Fox <BenjaminWFox@gmail.com>
2020-08-25 07:23:28 -0700

    Add AUTH_TENANT_GUID env var

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ module.exports = {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     AUTH_CLIENT_ID: process.env.AUTH_CLIENT_ID,
     AUTH_CLIENT_SECRET: process.env.AUTH_CLIENT_SECRET,
-    AUTH_TENANT_NAME: process.env.AUTH_TENANT_NAME,  
+    AUTH_TENANT_NAME: process.env.AUTH_TENANT_NAME,
+    AUTH_TENANT_GUID: process.env.AUTH_TENANT_GUID,
     JWT_SECRET: process.env.JWT_SECRET,
     USER_FLOW: process.env.USER_FLOW,
   }
--- pages/auth/signout.js
+++ pages/auth/signout.js
@@ -2,14 +2,7 @@ import { signOut } from 'next-auth/client'
 
 export default function Signout() {
   if (typeof window !== 'undefined') {
-    console.log('Is client - signing out')
-    console.log('*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***')
-
     signOut({ callbackUrl: process.env.NEXTAUTH_URL })
-  }
-  else {
-    console.log('Not client - not signing out')
-    console.log('*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***')
   }
 
   return null
